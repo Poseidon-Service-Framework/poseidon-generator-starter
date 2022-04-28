68637e85725ecaf375f9fa369bfbbeb399a0790f
muggle <isocket@outlook.com>
2022-04-28 22:50:58 +0800

    feat(1.0.0-release): 说明文档

--- src/main/java/com/muggle/controller/CodeController.java
+++ src/main/java/com/muggle/controller/CodeController.java
@@ -36,40 +36,41 @@ public class CodeController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CodeController.class);
 
 
-    @PostMapping(value = "/create",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String create(@RequestBody ProjectMessageVO projectMessageVO){
+    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String create(@RequestBody ProjectMessageVO projectMessageVO) {
         try {
-        final SimpleCodeGenerator simpleCodeGenerator = new SimpleCodeGenerator(convertMessage(projectMessageVO));
-        final CodeCommandInvoker invoker = new CodeCommandInvoker(simpleCodeGenerator);
+            final SimpleCodeGenerator simpleCodeGenerator = new SimpleCodeGenerator(convertMessage(projectMessageVO));
+            final CodeCommandInvoker invoker = new CodeCommandInvoker(simpleCodeGenerator);
 
-        invoker.popCommond("createPom");
-        if (!CollectionUtils.isEmpty(codeCommands)){
-            codeCommands.forEach(invoker::addCommond);
-        }
-        if (!projectMessageVO.getExcloudCommonds().contains("createUIpom")){
-            invoker.addCommond(new MyUIcodeCommand());
-        }
-        projectMessageVO.getExcloudCommonds().forEach(invoker::popCommond);
+            invoker.popCommond("createPom");
+            if (!CollectionUtils.isEmpty(codeCommands)) {
+                codeCommands.forEach(invoker::addCommond);
+            }
+            if (!projectMessageVO.getExcloudCommonds().contains("createUIpom")) {
+                invoker.addCommond(new MyUIcodeCommand());
+            }
+            projectMessageVO.getExcloudCommonds().forEach(invoker::popCommond);
             invoker.addCommond(new CodeCommand() {
                 @Override
                 public String getName() {
                     return "serializ";
                 }
+
                 @Override
                 public void excute(CodeGenerator codeGenerator) throws Exception {
                     StringBuilder path = new StringBuilder();
                     path.append(System.getProperty(USER_DIR)).append(SEPARATION).append("projectMessageVO.json");
                     final File file = new File(path.toString());
-                    if (!file.exists()){
+                    if (!file.exists()) {
                         try {
                             file.createNewFile();
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
-                    try (final FileOutputStream fileOutputStream = new FileOutputStream(file)){
+                    try (final FileOutputStream fileOutputStream = new FileOutputStream(file)) {
                         ObjectMapper mapper = new ObjectMapper();
-                        fileOutputStream.write( mapper.writeValueAsString(projectMessageVO).getBytes());
+                        fileOutputStream.write(mapper.writeValueAsString(projectMessageVO).getBytes());
                         LOGGER.info("==========> [持久化提交数据]");
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
@@ -78,28 +79,28 @@ public class CodeController {
                     }
                 }
             });
-        invoker.execute();
-        return "{\"result\":\"success\"}";
-        }catch (Exception e){
-            LOGGER.error(e.getMessage(),e);
-            return "{\"result\":\" 生成代码发生错误，错误原因："+e.getMessage()+"\"}";
+            invoker.execute();
+            return "{\"result\":\"success\"}";
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return "{\"result\":\" 生成代码发生错误，错误原因：" + e.getMessage() + "\"}";
         }
     }
 
 
-    @GetMapping(value = "/message",produces = MediaType.APPLICATION_JSON_UTF8_VALUE )
-    public String getMessage(){
+    @GetMapping(value = "/message", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getMessage() {
         StringBuilder path = new StringBuilder();
         path.append(System.getProperty(USER_DIR)).append(SEPARATION).append("projectMessageVO.json");
         final File file = new File(path.toString());
-        if (!file.exists()){
+        if (!file.exists()) {
             return "{}";
         }
-        try (final FileReader fileReader = new FileReader(file)){
-           BufferedReader buffer = new BufferedReader(fileReader);
+        try (final FileReader fileReader = new FileReader(file)) {
+            BufferedReader buffer = new BufferedReader(fileReader);
             String line = null;
             final StringBuilder result = new StringBuilder();
-            while((line = buffer.readLine())!=null) {
+            while ((line = buffer.readLine()) != null) {
                 result.append(line);
             }
             return result.toString();
@@ -119,12 +120,12 @@ public class CodeController {
             .suffix(projectMessageVO.getSuffix()).swagger(projectMessageVO.isSwagger())
             .tableName(projectMessageVO.getTableName()).username(projectMessageVO.getUsername())
             .module(projectMessageVO.getModule()).build();
-        if (CollectionUtils.isEmpty(build.getOtherField())){
+        if (CollectionUtils.isEmpty(build.getOtherField())) {
             final Map<String, String> map = new HashMap<>();
             build.setOtherField(map);
         }
-        if (build.getOtherField().get("parentVersion")==null){
-            build.getOtherField().put("parentVersion","1.0-SNAPSHOT");
+        if (build.getOtherField().get("parentVersion") == null) {
+            build.getOtherField().put("parentVersion", "1.0-SNAPSHOT");
         }
         return build;
     }
